6f722221a8cf9bbad80e56f04a6bc951c60df3cc
Pitchapa Wiangthong <69136823+PitchapaWiangthong@users.noreply.github.com>
2022-10-19 13:35:29 +0700

    solid principles

---
+++ Lab/book/book/BookPublisher.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class BookPublisher {
+    public static void main(String[] args) {
+        Book book = new Book("Tyland", List.of("I", "moved", "here", "recently", "too"));
+        PrintToFile file = new PrintToFile();
+        file.printToFile(book);
+    }
+}
